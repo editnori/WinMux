eadd75b3ae801b4f964da91150d13f3adf5f6c81
editnori <layth888@gmail.com>
2026-03-14 02:24:56 -0500

    Revert "Build workspace shell foundation"
    This reverts commit 09bccc104e54a3b36b56d5a244fac250ee6f3749.

--- SampleConfiguration.cs
+++ SampleConfiguration.cs
@@ -2,11 +2,28 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+
 namespace SelfContainedDeployment
 {
     internal static class SampleConfig
     {
         public const string FeatureName = "Native Terminal Starter";
         public static ElementTheme CurrentTheme = ElementTheme.Default;
+    }
+
+    public partial class MainPage : Page
+    {
+        private readonly List<Scenario> scenarios = new()
+        {
+            new Scenario() { Title = "Starter Overview", ClassName = typeof(Scenario1_SelfContainedDeployment).FullName }
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public string ClassName { get; set; }
     }
 }
